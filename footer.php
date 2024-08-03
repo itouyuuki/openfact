644a5f3296e92665a232f2d3ef88e6002670c1a3
itouyuuki <itou@nextreservation.jp>
2024-08-04 00:25:43 +0900

    tuika

--- footer.php
+++ footer.php
@@ -1,21 +1,15 @@
 <footer>
-	<div class="foot_main">
-		<div class="foot_1">
-			<img src="<?= $base ?>/logo.png" alt="NEXTRESERVATION株式会社" class="foot_logo"> 
-			<adress class="foot_about">
-				<div>〒386‐1211</div>
-				<div>長野県上田市下之郷348－1</div>
-			</adress>
-		</div>
-		<div class="foot_2">
-			<a href="<?= $base ?>">ホーム</a>
-			<a href="<?= $base ?>outline/">施工事例</a>
-			<a href="https://works.mekulo.jp/job/J2009901" target="_blank" rel="noopenner">よくある質問</a>
-			<a href="<?= $base ?>enter/">店舗情報</a>
-			<a href="<?= $base ?>blog/">来店予約</a>
-			<a href="<?= $base ?>contact/">お問い合わせ</a>
-		</div>
-		<div class="copyright">&copy;2024 NEXTRESERVATION株式会社</div>
+	<div class="fotbox">
+		<img src="fotlogo.png" alt="">
+		<ul>
+			<li><a>産業の陣とは</a></li>
+			<li><a>参加方法</a></li>
+			<li><a>城一覧</a></li>
+			<li><a>食事ガイド</a></li>
+			<li><a>公開映像</a></li>
+			<li><a>お問い合わせ</a></li>
+			<li><small>&copy;2024 産業の陣 運営委員会</small></li>
+		</ul>
 	</div>
 	<script src="hamburger.js"></script>
 </footer>	
--- index.php
+++ index.php
@@ -28,14 +28,107 @@ $og_image = '【TBD】';  //
 <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&family=Zen+Old+Mincho:wght@400;500;600;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
-<?php include $base . '/header.php' ?>
-    <div class="fv">
-    </div>
-    <section class="main">
-        <p>
-            NEXT RESERVATION株式会社は、長野県上田市のIT企業です。地元の上田市・東御市・坂城町の企業を中心に、ITを使用しマーケティング採用戦略、経営課題の解決を支援しています。また、長野県内の企業限定の求人サイト「めくろうワークス」を運営中です。
-            　どの事業も、対話からお客様を知ることを第一とし、丁寧な取材をして第三者目線の提案をおこなっています。
-        </p>
+<div class="top-back">
+    <?php include $base . '/header.php' ?>
+    <section id="fv" class="g">
+        <div class="fv-box">
+            <p>開幕まで<br>あと<b> 五十七 </b>日</p>
+            <img src="title.png" alt="">
+        </div>
+        <div class="eventdate">
+            <p>開催日：</p>
+            <p>2024年9月13日(金)、</p>
+            <p>14日(土)</p>
+        </div>
     </section>
+</div>
+<section id="intro">
+    <div class="intro-box">
+        <p>
+            ものづくりの
+        </p>
+        <div class="intro-text">
+            <p>「<b>仕事</b>に触れる」</p>
+            <p>「<b>技</b>に触れる」</p>
+            <p>「<b>思い</b>に触れる」</p>
+        </div>
+    </div>
+    <p>この秋、上田市と坂城町の工場が特別に解放される。</p>
+    <p>オープンファクトリーイベント「うえだ・さかき産業の陣」を開催。</p>
+    <p>工場見学やものづくり体験を通じて、</p>
+    <p>地元の誇るべき産業、こだわりの技術の数々、</p>
+    <p>職人たちの熱き魂をぜひ体感してください。</p>
+    <p>普段入ることが出来ない20社以上の工場が一斉に一般公開し、</p>
+    <p>あなたを待っています。</p>
+</section>
+<section class="open">
+    <div class="openback">
+        <p>
+            いざ、<br>
+            「うえだ・さかき産業の陣」<br>
+            <b>開幕</b>
+        </p>
+    </div>
+</section>
+<div class="backwindw"></div>
+<section class="company comsec">
+    <h2>参加企業一覧</h2>
+    <dl class="overview">
+        <div>
+            <dt>開催日</dt>
+            <dd>
+                2024年9月13日(金)，14日(土) ※企業によって公開日は異なります。
+            </dd>
+        </div>
+        <div>
+            <dt>開催時間</dt>
+            <dd>
+                各社の見学・体験時間　9:30～17:00  イベント受付時間　9:00～16:00　※企業によって公開時間は異なります。
+            </dd>
+        </div>
+        <div>
+            <dt>開催地</dt>
+            <dd>
+                長野県 上田市、坂城町の企業、飲食店など
+            </dd>
+        </div>
+        <div>
+            <dt>主催</dt>
+            <dd class="iinkai">
+                産業の陣 実行委員会
+            </dd>
+        </div>
+    </dl>
+</section>
+<section class="openview comsec">
+    <h2>開催概要</h2>
+    <dl class="overview">
+        <div>
+            <dt>開催日</dt>
+            <dd>
+                2024年9月13日(金)，14日(土) ※企業によって公開日は異なります。
+            </dd>
+        </div>
+        <div>
+            <dt>開催時間</dt>
+            <dd>
+                各社の見学・体験時間　9:30～17:00  イベント受付時間　9:00～16:00　※企業によって公開時間は異なります。
+            </dd>
+        </div>
+        <div>
+            <dt>開催地</dt>
+            <dd>
+                長野県 上田市、坂城町の企業、飲食店など
+            </dd>
+        </div>
+        <div>
+            <dt>主催</dt>
+            <dd class="iinkai">
+                産業の陣 実行委員会
+            </dd>
+        </div>
+    </dl>
+</section>
+<div class="backwindw"></div>
     <?php include $base . '/footer.php'; ?>
 </body>
